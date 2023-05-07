d13bd758aa7b919c660ce2f3f12f73f556e03714
Wenji Tong <wjtong@gmail.com>
2023-05-07 22:07:12 +0800

    keep working

--- src/main/java/com/banfftech/odata/EdmConfigLoader.java
+++ src/main/java/com/banfftech/odata/EdmConfigLoader.java
@@ -1,12 +1,32 @@
 package com.banfftech.odata;
 
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.apache.olingo.commons.api.edm.provider.CsdlEntityType;
 
+import java.util.List;
+
+@ApplicationScoped
 public class EdmConfigLoader {
     @Inject
     EdmConfig edmconfig;
 
-    public static void loadService(String serviceName) {
+    public EdmService loadService(String serviceName) {
+        EdmService edmService = new EdmService();
+        List<EdmConfig.OdataService> odataServiceList = edmconfig.services();
+        for (EdmConfig.OdataService odataService:odataServiceList) {
+            if (odataService.serviceName() == serviceName) {
+                List<String> entityTypes = odataService.entityType();
+                for (String entityType:entityTypes) {
+                    edmService.addEntityType(loadEntityType(entityType));
+                }
+            }
+        }
+        return edmService;
+    }
 
+    private CsdlEntityType loadEntityType(String entityType) {
+        CsdlEntityType csdlEntityType = new CsdlEntityType();
+        return csdlEntityType;
     }
 }
