906835b57ac0e55cca95087fa2b790c1016a6343
Wenji Tong <wtong@gmail.com>
2023-05-29 18:48:50 +0800

    working on expression visitor

--- src/main/java/com/banfftech/service/EntityService.java
+++ src/main/java/com/banfftech/service/EntityService.java
@@ -1,13 +1,31 @@
 package com.banfftech.service;
 
 import com.banfftech.model.GenericEntity;
+import com.banfftech.odata.processor.OdataExpressionVisitor;
+import org.apache.olingo.commons.api.http.HttpStatusCode;
+import org.apache.olingo.server.api.ODataApplicationException;
 import org.apache.olingo.server.api.uri.queryoption.FilterOption;
+import org.apache.olingo.server.api.uri.queryoption.expression.ExpressionVisitException;
 
 import java.util.List;
+import java.util.Locale;
 
 public class EntityService {
     // Based on the input parameter FilterOption which is OData query option, return the find result of the query
-    public static List<GenericEntity> findEntity(String entityName, FilterOption filterOption) {
+    public static List<GenericEntity> findEntity(String entityName, FilterOption filterOption)
+            throws ODataApplicationException {
+        OdataExpressionVisitor expressionVisitor = new OdataExpressionVisitor();
+        String hql = "from " + entityName;
+        String condition = null;
+        try {
+            if (filterOption != null) {
+                condition = (String) filterOption.getExpression().accept(expressionVisitor);
+                hql = hql + " where " + condition;
+            }
+        } catch (ExpressionVisitException e) {
+            throw new ODataApplicationException(e.getMessage(),
+                    HttpStatusCode.NOT_IMPLEMENTED.getStatusCode(), Locale.ENGLISH);
+        }
         return null;
     }
 }
